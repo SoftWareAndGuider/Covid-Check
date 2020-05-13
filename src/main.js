6a7c9c690c74669dafef8133785b5dae88d0b5c3
pmh-only <pmhstudio.pmh@gmail.com>
2020-05-13 15:05:01 +0900

    Stats

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
   $('#table').DataTable({
-    "scrollY": "500px",
+    "scrollY": "65vh",
     paging: false,
     order: [[ 1, 'asc' ], [ 0, 'asc' ]],
     colReorder: false,
     "ordering": false,
-    "info": false
+    "info": false,
+    "language": {
+      "search": "검색 <small>ex) 3학년 3반 체크안함</small>:"
+    }
   });
   $('.dataTables_length').addClass('bs-select');
+  $(".alert").alert();
 });
