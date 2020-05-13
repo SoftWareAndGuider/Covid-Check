a2738580b39aee1b04893f60c3d67d65226ca8c4
pmh-only <pmhstudio.pmh@gmail.com>
2020-05-13 19:39:26 +0900

    Checker
    Co-authored-by: TriNet / NoeulNight <lhj082926@gmail.com>

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const db = knex({
   }
 })
 
+app.use('/api', express.json({ limit: '500K' }))
 app.use('/src', express.static(path() + '/src/'))
 
 app.get('/', (_, res) => res.redirect('/main'))
@@ -28,6 +29,46 @@ app.get('/main', (_, res) => {
     })
   })
 })
+
+app.put('/api', apiHandle)
+app.put('/api/v1', apiHandle)
+
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
+function apiHandle (req, res) {
+  const { process, ...body } = req.body
+  if (!process) return res.status(406).send('data "process" not found')
+  switch (process) {
+    case 'check': {
+      if (!body.id) return res.status(406).send('data "id" not found')
+      db.update({ checked: 1 }).where('id', body.id).from('checks').then(() => {
+        db.select('*').where('id', body.id).from('checks').then(([data]) => {
+          if (!data) return res.send({ success: false })
+          res.send({ success: true, data: data })
+        })
+      })
+      break
+    }
+
+    case 'uncheck': {
+      if (!body.id) return res.status(406).send('data "id" not found')
+      db.update({ checked: 0 }).where('id', body.id).from('checks').then(() => {
+        db.select('*').where('id', body.id).from('checks').then((data) => {
+          if (!data) return res.send({ success: false })
+          res.send({ success: true, data })
+        })
+      })
+      break
+    }
+
+    default: {
+      res.status(406).send('data "process" not found')
+      break
+    }
+  }
+}
 
 app.listen(port, () => {
   console.log('Server is now on http://localhost:' + port)
--- src/main.js
+++ src/main.js
@@ -1,15 +1,49 @@
 $(document).ready(function () {
   $('#table').DataTable({
-    "scrollY": "65vh",
+    "scrollY": "50vh",
     paging: false,
     order: [[ 1, 'asc' ], [ 0, 'asc' ]],
     colReorder: false,
+    "processing": true,
     "ordering": false,
     "info": false,
     "language": {
       "search": "검색 <small>ex) 3학년 3반 체크안함</small>:"
+    },
+    "search": {
+      "search": localStorage.getItem('search') || ""
     }
   });
   $('.dataTables_length').addClass('bs-select');
   $(".alert").alert();
+  document.querySelector("input[type=search]").addEventListener("input", () => {
+    localStorage.setItem("search", document.querySelector("input[type=search]").value)
+  })
+  setTimeout(() => {
+    window.location.reload()
+  }, 30000)
 });
+
+function check (id) {
+  const req = new XMLHttpRequest()
+  req.open('PUT', '/api')
+  req.setRequestHeader("Content-Type", "application/json");
+  req.send(JSON.stringify({
+    id, process: "check"
+  }))
+  req.onload = () =>{
+    window.location.reload()
+  }
+}
+
+function uncheck (id) {
+  const req = new XMLHttpRequest()
+  req.open('PUT', '/api')
+  req.setRequestHeader("Content-Type", "application/json");
+  req.send(JSON.stringify({
+    id, process: "uncheck"
+  }))
+  req.onload = () =>{
+    window.location.reload()
+  }
+}
